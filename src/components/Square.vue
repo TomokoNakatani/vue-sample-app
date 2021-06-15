fb93415cdebc963d078dfe55da87698feae008d2
TomokoNakatani <67673484+TomokoNakatani@users.noreply.github.com>
2021-06-15 10:36:22 +0900

    アラートを表示する

--- src/components/Square.vue
+++ src/components/Square.vue
@@ -1,10 +1,15 @@
 <template>
-  <button class="square">{{ value }}</button>
+  <button class="square" @click="click">{{ value }}</button>
 </template>
 
 <script>
 export default {
   name: "Square",
   props: { value: Number },
+  methods: {
+    click() {
+      alert("click");
+    },
+  },
 };
 </script>
